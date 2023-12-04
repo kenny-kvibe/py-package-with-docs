5bcc9e7b030d96e0c9e1bbe08558c7fbc9d24c83
~KVibe <16114808+kenny-kvibe@users.noreply.github.com>
2023-12-04 09:00:26 +0100

    Added `run_daemon` to `mod.py`

--- app/module2/mod.py
+++ app/module2/mod.py
@@ -12,3 +12,16 @@ def use_function_mul():
 def use_function_div():
 	""" Using the `utils` module `function_div` function """
 	print('>>> Result:', utils.function_div(NUMBER_A, NUMBER_B))
+
+def run_daemon():
+	""" Run the daemon - non-exit process, prints per second """
+	i = 0
+	loop = True
+	try:
+		while loop:
+			i += 1
+			print(f'>>> Loop: {i}')
+			time.sleep(1)
+	except KeyboardInterrupt:
+		print('Exiting ...')
+		loop = False
