c0989bdb7c56fde36e6ba54be6be634ad7366605
~KVibe <16114808+kenny-kvibe@users.noreply.github.com>
2023-12-04 09:04:00 +0100

    Added `run_daemon` to `__main__.py`

--- app/module2/__main__.py
+++ app/module2/__main__.py
@@ -1,12 +1,13 @@
 """ Module `module2` main """
 
-from . import use_function_mul, use_function_div
+from . import use_function_mul, use_function_div, run_daemon
 
 
 def main() -> int:
 	""" Module `module2` main function """
 	use_function_mul()
 	use_function_div()
+	run_daemon()
 	return 0
 
 
